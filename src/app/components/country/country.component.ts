cbbb2b7a8f0708cdb346eae8eb7322ebd92646c7
Erandi <erandikd@curbfood.com>
2022-01-27 13:52:39 +0100

    route navigation fixed for edit country

--- src/app/components/country/country.component.ts
+++ src/app/components/country/country.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { ContinentItem } from 'src/app/model/continent-item.model';
 import { ContinentService } from 'src/app/service/continent.service';
 
@@ -9,26 +10,33 @@ import { ContinentService } from 'src/app/service/continent.service';
   styleUrls: ['./country.component.scss'],
 })
 export class CountryComponent implements OnInit {
+  continentList: ContinentItem[] = [];
+  selectedContinentId?: number;
+  query?: string;
+  showCountryHeader: boolean = true;
+
   constructor(
     private continentService: ContinentService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) {
     this.router.navigate([], {
       queryParams: {
         continentId: null,
         query: null,
       },
-      queryParamsHandling: 'merge'
-    })
+      queryParamsHandling: 'merge',
+    });
     this.getContinentList();
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (val.url.indexOf('/country/list') != -1)
+          this.showCountryHeader = true;
+      }
+    });
   }
 
-  continentList: ContinentItem[] = [];
-  selectedContinentId?: number;
-  query?: string;
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   sumbitCountrySearch(): void {
     var searchParams = {
@@ -36,6 +44,11 @@ export class CountryComponent implements OnInit {
       query: this.query,
     };
     this.router.navigate([], { queryParams: searchParams });
+  }
+
+  navigateToAddNewCountry(): void {
+    this.showCountryHeader = false;
+    this.router.navigate(['edit-item'], { relativeTo: this.activatedRoute });
   }
 
   private getContinentList(): void {
